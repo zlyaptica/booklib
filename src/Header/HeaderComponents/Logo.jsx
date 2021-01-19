7bf7253d7ada168eeeefc76353fa3a8e38206585
zlyaptica <maximov021@mail.ru>
2021-01-19 17:57:09 +0300

    refactor header`s styles

--- src/Header/HeaderComponents/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import logo from '../img/logo.svg'
-
-const Logo = () => {
-    return (
-        <a href="#" className="header__home"><img src={logo} alt="logo" className="header__logo"></img></a>
-    )
-}
-
-export { Logo }
